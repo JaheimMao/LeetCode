2c4d6ab068a8ae410dae80c52b1bed18c54ca7c1
JaheimMao <jiahuimao@outlook.com>
2021-03-13 22:23:40 +0800

    Create Add Two Numbers

---
+++ 0002_Add Two Numbers/0002_Question.md
@@ -0,0 +1,8 @@
+# 2 Add Two Number
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/dd-two-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
---
+++ 0002_Add Two Numbers/0002_Solution.cpp
@@ -0,0 +1,51 @@
+//2.Add Two Numbers
+#include<iostream>
+
+using namespace std;
+
+/* Definition for singly-linked list.*/
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *result, *p1 = l1, *p2 = l2, *p ;
+        int carry = 0;
+        int temp = 0;
+        result = new ListNode();
+        p = result;
+        while(p1 || p2){
+            if(p1 == nullptr){
+                temp = carry + p2->val;
+                p2 = p2->next;
+            }
+                
+            else if(p2 == nullptr){
+                temp = carry + p1->val;
+                p1 = p1->next;
+            }
+            else{
+                temp = carry + p1->val + p2->val;
+                p1 = p1->next;
+                p2 = p2->next;
+            }
+            if(temp > 9){
+                carry = 1;
+                temp = temp % 10;
+            }
+            else
+                carry = 0;
+            p->next = new ListNode(temp);
+            p = p->next;
+        }
+        if(carry)
+            p->next = new ListNode(1);
+        return result->next;
+    }
+};
