e7b54a7a869aeae9370300c22629bb036131c079
JaheimMao <jiahuimao@outlook.com>
2021-03-13 16:22:39 +0800

    Two Sum

--- 1_Two Sum/Solution.cpp
+++ 1_Two Sum/Solution.cpp
@@ -2,11 +2,12 @@
 // 
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 
 using namespace std;
 
 
-/*******暴力解法，用两层循环遍历求和**************/
+/*******暴力解法，用两层循环遍历求和**************
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -17,7 +18,8 @@ public:
         return {0};
     }
 };
-/***********************************************/
+***********************************************/
+
 
 /*************使用哈希表提高查找速率**************/
 class Solution {
@@ -25,11 +27,11 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> Hash;
         for(int i = 0; i < nums.size(); i++){
-            int temp = target - nums[i];            
-            auto it = Hash.find(temp);
+            int temp = target - nums[i]; //获取目标值和当前读取值之间的差
+            auto it = Hash.find(temp); //判断差值是否在Hash表中
             if(it != Hash.end())
                 return {it->second,i};
-            Hash[nums[i]] = i;
+            Hash[nums[i]] = i; //将当前值存入Hash表
         }
         return {0};
     }
@@ -37,6 +39,18 @@ public:
 /*********************************************/
 
 int main(void){
+    Solution test;
+    vector<int> nums;
+    vector<int> result;
+    int target;
+    int temp;
+
+    nums = {3,6,7};
+    target = 13;
+
+    result = test.twoSum(nums, target);
+    for(int i=0; i<result.size(); i++)
+        cout << result[i]; 
     
     return 0;
 }
