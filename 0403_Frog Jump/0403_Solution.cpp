3e72cc0a12cf887898d6f80852129d79e1a2e6c2
JaheimMao <jiahuimao@outlook.com>
2021-04-29 20:06:06 +0800

    Update 0403

--- 0403_Frog Jump/0403_Solution.cpp
+++ 0403_Frog Jump/0403_Solution.cpp
@@ -6,27 +6,33 @@ using namespace std;
 
 class Solution {
 public:
-    bool dfs(vector<vector<int>> dp, int i, int k){
-        if(i == 0){
-            k = 0;
-            dp[i][k] = true;
-            return true;
-        }
-        else if(i == 1){
-            k = 1;
-            dp[i][k] =true;
-            return true;
-        }
-        else if(!dp[i-k-1][k+1] && !dp[i-k][k] && !dp[i-k+1][k-1])
-            return false;
-        else {
-        }
-    }
     bool canCross(vector<int>& stones) {
-
+        int stonesSize = stones.size();
+        vector<vector<int>> dp(stonesSize, vector<int>(stonesSize));
+        dp[0][0] = true;
+        for(int i=1; i<stonesSize; i++)
+            if(stones[i]-stones[i-1] > i){
+                cout<<"\ni = "<<i<<endl;
+                return false;
+            }
+        
+        for(int i=1; i<stonesSize; i++)
+            for(int j=i-1;j>=0; j--){
+                int k = stones[i]-stones[j];
+                if(k>j+1)
+                    break;
+                dp[i][k] = dp[j][k+1] || dp[j][k] || dp[j][k-1];
+                cout<<"i = "<<i<<", j = "<<j<<", k = "<<k<<endl;
+                if(i == stonesSize-1 && dp[i][k])
+                    return true;
+            }
+        return false;
     }
 };
 
 int main(void){
+    Solution test;
+    vector<int> stones = {0,1,3,6,10,15,21,28};
+    cout<<"\nThe result is "<<test.canCross(stones)<<endl;
     return 0;
 }
